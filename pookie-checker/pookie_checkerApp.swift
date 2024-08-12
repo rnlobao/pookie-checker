6571d0b83b5a1a0624ae04d7721865e6272302d5
Robson Novato <rnlobao42@gmail.com>
2024-08-12 15:33:07 -0300

    Initial commit

---
+++ pookie-checker/pookie_checkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pookie_checkerApp.swift
+//  pookie-checker
+//
+//  Created by Robson Novato on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct pookie_checkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
